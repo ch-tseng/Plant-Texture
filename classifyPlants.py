7a402788e7854a8c5dd1ea450c2bced47adce928
ch-tseng <ch.tseng@sunplusit.com>
2017-03-24 23:15:59 -0400

    update

--- classifyPlants.py
+++ classifyPlants.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import the necessary packages
 from __future__ import print_function
-#from sklearn.neighbors import KNeighborsClassifier
-#from sklearn.cross_validation import train_test_split
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.tree import DecisionTreeClassifier
-#from sklearn.svm import SVC
-#from sklearn.linear_model import LogisticRegression
 from skimage import feature
 from imutils import paths
 import numpy as np
@@ -48,7 +42,6 @@ def describe(image, ftype="Hara"):
             return np.hstack([colorStats, hist])
         
 
-# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--type", required=True, help="LBA or Hara?")
 ap.add_argument("-d", "--dataset", required=True, help="path to 8 scene category dataset")
@@ -56,22 +49,15 @@ ap.add_argument("-f", "--forest", type=int, default=-1,
 	help="whether or not a Random Forest should be used")
 args = vars(ap.parse_args())
 
-# grab the set of image paths and initialize the list of labels and matrix of
-# features
 print("[INFO] extracting features...")
 imagePaths = sorted(paths.list_images(args["dataset"]))
 labels = []
 data = []
 
-# loop over the images in the input directory
 for imagePath in imagePaths:
-	# extract the label and load the image from disk
-	#label = imagePath[imagePath.rfind("/") + 1:].split("_")[0]
         label = imagePath.split("/")[-2]
 	image = cv2.imread(imagePath)
 
-	# extract features from the image, then update the list of lables and
-	# features
 	features = describe(image)
 	labels.append(label)
 	data.append(features)
@@ -81,19 +67,7 @@ for imagePath in imagePaths:
 (trainData, testData, trainLabels, testLabels) = train_test_split(np.array(data),
 	np.array(labels), test_size=0.20, random_state=42)
 
-# initialize the model as a decision tree
-#model = DecisionTreeClassifier(criterion='gini', random_state=84)
-#model = DecisionTreeClassifier(criterion='entropy', random_state=84)
 model = RandomForestClassifier(n_estimators=args['forest'], random_state=42)
-#model = KNeighborsClassifier(n_neighbors=30)
-#model = SVC(kernel="linear")
-#model = SVC(kernel="poly", degree=2, coef0=1)
-#model = SVC(kernel="rbf")
-#model = LogisticRegression()
-# check to see if a Random Forest should be used instead
-#if args["forest"] > 0:
-#        model = RandomForestClassifier(n_estimators=80, random_state=42)
-
 
 # train the decision tree
 print("[INFO] training model...")
@@ -104,9 +78,7 @@ print("[INFO] evaluating...")
 predictions = model.predict(testData)
 print(classification_report(testLabels, predictions))
 
-# loop over a few random images
 for i in np.random.randint(0, high=len(imagePaths), size=(10,)):
-	# grab the image and classify it
 	imagePath = imagePaths[i]
 	filename = imagePath[imagePath.rfind("/") + 1:]
 	image = cv2.imread(imagePath)
